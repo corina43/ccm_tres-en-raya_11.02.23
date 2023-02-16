001e3b98f63aadb7fa57663087e7115fd4d8e3c7
corina <ciorpaccorina@gmail.com>
2023-02-16 01:03:03 +0100

    conseguido pintar tres fichas,prueba mensaje ganador por modal no conseguido

--- js/game.js
+++ js/game.js
@@ -1,16 +1,9 @@
- 
-
-
-      
 const inputNamePlayer1 = sessionStorage.getItem("namePlayer1");
 const inputNamePlayer2 = sessionStorage.getItem("namePlayer2");
 
 document.getElementById("player1").innerHTML = inputNamePlayer1;
 document.getElementById("player2").innerHTML = inputNamePlayer2;
 
-
-// let namePlayer1 = 3;
-// let namePlayer2 = 3;
 let remainingPieces = {
   player1: 3,
   player2: 3,
@@ -22,20 +15,26 @@ for (let i = 0; i < boxes.length; i++) {
   let active = "active" + i;
   boxes[i].addEventListener("click", function () {
     if (!this.innerHTML) {
-      if (currentPlayer === "player1") {
-        this.innerHTML = "X";
-        currentPlayer = "player2";
-        document.getElementById("player1").style.backgroundColor = "lightgray";
-        document.getElementById("player2").style.backgroundColor = "yellow";
-        changeTurn();
-        checkWinner();
+      if (remainingPieces[currentPlayer] > 0) {
+        if (currentPlayer === "player1") {
+          this.innerHTML = "X";
+          remainingPieces.player1--;
+          currentPlayer = "player2";
+          document.getElementById("player1").style.backgroundColor = "lightgray";
+          document.getElementById("player2").style.backgroundColor = "yellow";
+          changeTurn();
+          checkWinner();
+        } else {
+          this.innerHTML = "O";
+          remainingPieces.player2--;
+          currentPlayer = "player1";
+          document.getElementById("player2").style.backgroundColor = "lightgray";
+          document.getElementById("player1").style.backgroundColor = "yellow";
+          changeTurn();
+          checkWinner();
+        }
       } else {
-        this.innerHTML = "O";
-        currentPlayer = "player1";
-        document.getElementById("player2").style.backgroundColor = "lightgray";
-        document.getElementById("player1").style.backgroundColor = "yellow";
-        changeTurn();
-        checkWinner();
+        alert("Ya no tienes fichas para colocar");
       }
     }
   });
@@ -79,25 +78,45 @@ function checkWinner(){
   if (board.every(box => box !== "")) {
     declareDraw();
   }
-
 }
+
 function declareWinner(player) {
   winner = player;
-  document.getElementById("turn").innerHTML = `Ganador: ${winner}`;
-  boxes.forEach(box => {
-    box.removeEventListener("click", changeTurn);
-  });
+  alert(`¡Felicidades ${winner}! ¡Has ganado!`);
+  resetGame();
 }
 
 function declareDraw() {
-  document.getElementById("turn").innerHTML = "Empate";
-  boxes.forEach(box => {
-    box.removeEventListener("click", changeTurn);
-  });
+  alert("¡Empate!");
+  resetGame();
 }
 
-  
+function resetGame() {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+  }
+  remainingPieces = {
+    player1: 3,
+    player2: 3,
+  };
+  currentPlayer = "player1";
+  turn = 1;
+  winner = "";
+  document.getElementById("turn").innerHTML = `Turno de ${inputNamePlayer1}`;
+  document.getElementById("player1").style.backgroundColor = "yellow";
+  document.getElementById("player2").style.backgroundColor
 
 
+}
+// Marco las posiciones ganadoras y muestro el modal de victoria
+// function winner(winningCombinations){
+//     console.log(winningCombinations)
+//     winningCombinations.forEach(board => boxes[board].classList.toggle("ganador",true));
+//     mostrarModal("Ganador jugador " + (estadoJuego === "P1" ? "2" : "1"));
+// }
 
-
+// function mostrarModal(texto){
+//     textoModal.innerText = texto;
+//     modal.showModal();
+//     estadoJuego = "PAUSA";
+// }
