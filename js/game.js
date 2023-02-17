8179e9abd7f33426d70f612050598fd0c76c9745
corina <ciorpaccorina@gmail.com>
2023-02-17 10:35:56 +0100

    modificacion de funcion remainingPieces ,cambio de funcionalidades

--- js/game.js
+++ js/game.js
@@ -4,108 +4,80 @@ const inputNamePlayer2 = sessionStorage.getItem("namePlayer2");
 document.getElementById("player1").innerHTML = inputNamePlayer1;
 document.getElementById("player2").innerHTML = inputNamePlayer2;
 
-
-var player1 = "X";
-  var player2 = "O";
-  var currentPlayer = player1;
-  var boxes = document.querySelectorAll('.box');
-  
-  for (var i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', function(event) {
-      if (event.target.innerHTML === "") {
-        event.target.innerHTML = currentPlayer;
-        checkWinner();
-        switchPlayer();
-      }
-    });
-  }
-  
-  function switchPlayer() {
-    if (currentPlayer === player1) {
-      currentPlayer = player2;
-    } else {
-      currentPlayer = player1;
-    }
-  }
-  
-  function checkWinner() {
-    if (
-      boxes[0].innerHTML === currentPlayer && 
-      boxes[1].innerHTML === currentPlayer && 
-      boxes[2].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[3].innerHTML === currentPlayer && 
-      boxes[4].innerHTML === currentPlayer && 
-      boxes[5].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[6].innerHTML === currentPlayer && 
-      boxes[7].innerHTML === currentPlayer && 
-      boxes[8].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[0].innerHTML === currentPlayer && 
-      boxes[3].innerHTML === currentPlayer && 
-      boxes[6].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[1].innerHTML === currentPlayer && 
-      boxes[4].innerHTML === currentPlayer && 
-      boxes[7].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[2].innerHTML === currentPlayer && 
-      boxes[5].innerHTML === currentPlayer && 
-      boxes[8].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[0].innerHTML === currentPlayer && 
-      boxes[4].innerHTML === currentPlayer && 
-      boxes[8].innerHTML === currentPlayer
-    ) {
-      alert("El jugador " + currentPlayer + " ha ganado!");
-      resetGame();
-    } else if (
-      boxes[2].innerHTML === currentPlayer && 
-      boxes[4].innerHTML === currentPlayer &&
-      boxes[6].innerHTML === currentPlayer 
-    )
-        alert("El jugador " + currentPlayer + " ha ganado!");
-        resetGame();
-  }
-  var turn = "player1"; // establece el turno inicial en player1
-var boxes = document.querySelectorAll(".box"); // selecciona todas las cajas
-
-for (var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener("click", function(event) {
-    if (event.target.innerHTML === "") {
-      event.target.innerHTML = turn;
-      if (turn === "player1") {
-        turn = "player2";
+let remainingPieces = {
+  player1: 3,
+  player2: 3,
+	 for(let i = 0; i < boxes.length; i++){
+  let active = "active" + i;
+  boxes[i].addEventListener("click", function () {
+    if (!this.innerHTML) {
+      if (remainingPieces[currentPlayer] > 0) {
+        if (currentPlayer === "player1") {
+          this.innerHTML = "X";
+          remainingPieces.player1--;
+          currentPlayer = "player2";
+          
+          document.getElementById("player1").style.backgroundColor = "lightgray";
+          document.getElementById("player2").style.backgroundColor = "yellow";
+          changeTurn();
+          checkWinner();
+        } else {
+          this.innerHTML = "O";
+          remainingPieces.player2--;
+          currentPlayer = "player1";
+          document.getElementById("player2").style.backgroundColor = "lightgray";
+          document.getElementById("player1").style.backgroundColor = "yellow";
+          changeTurn();
+          checkWinner();
+        }
       } else {
-        turn = "player1";
+        alert("Ya no tienes fichas para colocar");
       }
     }
   });
+  function checkWinner(){
+  if (board.every(box => box !== "")) {
+    declareDraw();
+  }
 }
 
-// // función para reiniciar el juego
-// document.querySelector("button[type='reset']").addEventListener("click", function() {
-//   for (var i = 0; i < boxes.length; i++) {
-//     boxes[i].innerHTML = "";
-//   }
-//   turn = "player1";
-// });
+function declareWinner(player) {
+  winner = player;
+  alert(`¡Felicidades ${winner}! ¡Has ganado!`);
+  resetGame();
+}
+
+function declareDraw() {
+  alert("¡Empate!");
+  resetGame();
+}
+
+function resetGame() {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+  }
+  remainingPieces = {
+    player1: 3,
+    player2: 3,
+  };
+  currentPlayer = "player1";
+  turn = 1;
+  winner = "";
+  document.getElementById("turn").innerHTML = `Turno de ${inputNamePlayer1}`;
+  document.getElementById("player1").style.backgroundColor = "yellow";
+  document.getElementById("player2").style.backgroundColor
+
+
+}
+// Marco las posiciones ganadoras y muestro el modal de victoria
+// function winner(winningCombinations){
+//     console.log(winningCombinations)
+//     winningCombinations.forEach(board => boxes[board].classList.toggle("ganador",true));
+//     mostrarModal("Ganador jugador " + (estadoJuego === "P1" ? "2" : "1"));
+// }
+
+// function mostrarModal(texto){
+//     textoModal.innerText = texto;
+//     modal.showModal();
+//     estadoJuego = "PAUSA";
+// }
