b1bc7ac3203273d58619ee6c4d68391d42998442
corina <ciorpaccorina@gmail.com>
2023-02-19 00:18:02 +0100

    Obtengo los elementos de las cajas del tablero y el cuadro de diálogo del juego

--- js/game.js
+++ js/game.js
@@ -1,145 +1,48 @@
+// Obtengo los nombres de los jugadores desde el almacenamiento de sesión
 const inputNamePlayer1 = sessionStorage.getItem("namePlayer1");
 const inputNamePlayer2 = sessionStorage.getItem("namePlayer2");
 
+// Asigno los nombres de los jugadores a los elementos correspondientes en la página
 document.getElementById("player1").innerHTML = inputNamePlayer1;
 document.getElementById("player2").innerHTML = inputNamePlayer2;
 
-// Caracteres para el tablero
+// Defino los caracteres para el tablero
 const x = "✖";
 const o = "〇";
 
-// Elementos de la página
+// Obtengo los elementos de las cajas del tablero y el cuadro de diálogo del juego
 const boxes = document.querySelectorAll(".box");
 const modal = document.querySelector("dialog");
 const textoModal = modal.querySelector("h2");
+
+// Establezco el estado inicial del juego y muestro el primer turno
 let estadoJuego = "player1"; // player1 | player2 | PAUSA
-let contadorFichasPlayer1 = 3;
-let contadorFichasPlayer2 = 3;
-
 const turno = document.getElementById("turn");
-turno.innerHTML = "Turno de " + inputNamePlayer1 + " (" + contadorFichasPlayer1 + " fichas restantes)";
+turno.innerHTML = "Turno de " + inputNamePlayer1;
 
+// Añado un event listener a cada caja del tablero
 boxes.forEach((box, posicion) => {
   box.addEventListener("click", () => {
+    // Si el juego está en pausa, no se permite hacer jugadas
     if (estadoJuego === "PAUSA") return;
+    // Si la caja ya está ocupada, no se permite hacer jugadas
     if (box.textContent !== "") return;
-    if (estadoJuego === "player1" && contadorFichasPlayer1 === 0) return;
-    if (estadoJuego === "player2" && contadorFichasPlayer2 === 0) return;
+    // Si la caja está libre, se coloca la ficha correspondiente al jugador actual
     box.textContent = estadoJuego === "player1" ? x : o;
+    // Se cambia al siguiente jugador
     estadoJuego = estadoJuego === "player1" ? "player2" : "player1";
-    if (estadoJuego === "player1") {
-      contadorFichasPlayer1--;
-      turno.innerHTML = "Turno de " + inputNamePlayer1 + " (" + contadorFichasPlayer1 + " fichas restantes)";
-    } else {
-      contadorFichasPlayer2--;
-      turno.innerHTML = "Turno de " + inputNamePlayer2 + " (" + contadorFichasPlayer2 + " fichas restantes)";
-    }
+    // Se revisa si hay un ganador en la jugada actual
     const posicionGanadora = revisarSiHayGanador();
     if (Array.isArray(posicionGanadora)) {
+      // Si hay un ganador, se resaltan las posiciones ganadoras y se muestra el modal de victoria
       ganar(posicionGanadora);
       return;
     }
     if (posicionGanadora === "empate") {
+      // Si hay empate, se muestra el modal correspondiente
       mostrarModal("Empate");
     }
+    // Se actualiza el turno en la página con el jugador actual
+    turno.innerHTML = "Turno de " + (estadoJuego === "player1" ? inputNamePlayer1 : inputNamePlayer2);
   });
 });
-
-modal.querySelector("button").addEventListener("click", () => {
-  boxes.forEach(box => {
-    box.textContent = "";
-    box.classList.toggle("ganador", false);
-  });
-  modal.close();
-  estadoJuego = "player1";
-  contadorFichasPlayer1 = 3;
-  contadorFichasPlayer2 = 3;
-  turno.innerHTML = "Turno de " + inputNamePlayer1 + " (" + contadorFichasPlayer1 + " fichas restantes)";
-});
-
-
-function revisarSiHayGanador() {
-  const tablero = Array.from(boxes).map(box => box.textContent);
-  let fichasJugador1 = 0;
-  let fichasJugador2 = 0;
-  
-  // Reviso filas
-  for (let i = 0; i < 9; i+=3) {
-    fichasJugador1 = 0;
-    fichasJugador2 = 0;
-    for (let j = i; j < i+3; j++) {
-      if (tablero[j] === x) {
-        fichasJugador1++;
-      } else if (tablero[j] === o) {
-        fichasJugador2++;
-      }
-    }
-    if (fichasJugador1 === 3) {
-      return [i, i+1, i+2];
-    } else if (fichasJugador2 === 3) {
-      return [i, i+1, i+2];
-    }
-  }
-  
-  // Reviso columnas
-  for (let i = 0; i < 3; i++) {
-    fichasJugador1 = 0;
-    fichasJugador2 = 0;
-    for (let j = i; j < i+7; j+=3) {
-      if (tablero[j] === x) {
-        fichasJugador1++;
-      } else if (tablero[j] === o) {
-        fichasJugador2++;
-      }
-    }
-    if (fichasJugador1 === 3) {
-      return [i, i+3, i+6];
-    } else if (fichasJugador2 === 3) {
-      return [i, i+3, i+6];
-    }
-  }
-  
-  // Reviso diagonales
-  fichasJugador1 = 0;
-  fichasJugador2 = 0;
-  for (let i = 0; i < 9; i+=4) {
-    if (tablero[i] === x) {
-      fichasJugador1++;
-    } else if (tablero[i] === o) {
-      fichasJugador2++;
-    }
-  }
-  if (fichasJugador1 === 3) {
-    return [0, 4, 8];
-  } else if (fichasJugador2 === 3) {
-    return [0, 4, 8];
-  }
-  fichasJugador1 = 0;
-  fichasJugador2 = 0;
-  for (let i = 2; i < 7; i+=2) {
-    if (tablero[i] === x) {
-      fichasJugador1++;
-    } else if (tablero[i] === o) {
-      fichasJugador2++;
-    }
-  }
-  if (fichasJugador1 === 3) {
-    return [2, 4, 6];
-  } else if (fichasJugador2 === 3) {
-    return [2, 4, 6];
-  }
-  
-  // Reviso empate
-  if (tablero.includes("")) return false;
-  return "empate";
-}
-//Marco las posiciones ganadoras y muestro el modal de victoria
-function ganar(posicionesGanadoras){  
-  posicionesGanadoras.forEach(posicion => boxes[posicion].classList.toggle("ganador",true));
-  mostrarModal("Ganador  " + (estadoJuego === "player1" ? `${inputNamePlayer2}` : `${inputNamePlayer1}`));
-}
-function mostrarModal(texto){
- textoModal.innerText = texto;
- modal.showModal();
- estadoJuuego = "PAUSA";
-}
