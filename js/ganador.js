bdb01afd04cb81f0a5d2184be041e1fe1a2c9b49
corina <ciorpaccorina@gmail.com>
2023-02-22 09:38:12 +0100

    areglo skroll

--- js/ganador.js
+++ js/ganador.js
@@ -7,13 +7,13 @@ const winner = sessionStorage.getItem("winner");
       document.getElementById("player1").innerHTML = namePlayer1;
       document.getElementById("player2").innerHTML = namePlayer2;
       
-      if (winner) {
-          // Si hay un ganador, mostramos su nombre en el título
-          document.querySelector("h1").textContent = `¡Felicidades ${winner}! ¡Has ganado!`;
-      } else if (isDraw) {
-          // Si hubo empate, mostramos un mensaje indicando que fue un empate
-          document.querySelector("h1").textContent = "¡Es un empate!";
-      }
+    //   if (winner) {
+    //       // Si hay un ganador, mostramos su nombre en el título
+    //       document.querySelector("h1").textContent = `¡Felicidades ${winner}! ¡Has ganado!`;
+    //   } else if (isDraw) {
+    //       // Si hubo empate, mostramos un mensaje indicando que fue un empate
+    //       document.querySelector("h1").textContent = "¡Es un empate!";
+    //   }
       
       // Agregamos un listener al botón para que reinicie el juego
       document.querySelector("button").addEventListener("click", () => {
